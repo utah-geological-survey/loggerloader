98749042e722c26d2ddde37f3fe2b92ecedc1b37
Paul <paulinkenbrandt@utah.gov>
2017-07-18 11:44:08 -0600

    Create arcstuff.py

---
+++ loggerloader/arcstuff.py
@@ -0,0 +1,33 @@
+import arcpy
+
+def get_field_names(table):
+    read_descr = arcpy.Describe(table)
+    field_names = []
+    for field in read_descr.fields:
+        field_names.append(field.name)
+    field_names.remove('OBJECTID')
+    return field_names
+
+
+def table_to_pandas_dataframe(table, field_names=None, query=None):
+    """
+    Load data into a Pandas Data Frame for subsequent analysis.
+    :param table: Table readable by ArcGIS.
+    :param field_names: List of fields.
+    :return: Pandas DataFrame object.
+    """
+    # if field names are not specified
+    if not field_names:
+        field_names = get_field_names(table)
+    # create a pandas data frame
+    df = pd.DataFrame(columns=field_names)
+
+    # use a search cursor to iterate rows
+    with arcpy.da.SearchCursor(table, field_names,query) as search_cursor:
+        # iterate the rows
+        for row in search_cursor:
+            # combine the field names and row items together, and append them
+            df = df.append(dict(zip(field_names, row)),ignore_index=True)
+
+    # return the pandas data frame
+    return df
