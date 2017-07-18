62a5220f3a40e81f7f498ca90491beac9a154c24
Paul <paulinkenbrandt@utah.gov>
2017-07-18 10:47:39 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ import os
 from setuptools import setup, find_packages
 
 if not sys.version_info[0] in [2,3]:
-    print('Sorry, wellapplication not supported in your Python version')
+    print('Sorry, loggerloader not supported in your Python version')
     print('  Supported versions: 2 and 3')
     print('  Your version of Python: {}'.format(sys.version_info[0]))
     sys.exit(1)  # return non-zero value for failure
 
-long_description = 'A tool for hydrogeologists to upload and display hydrographs and geochemical data'
+long_description = 'A tool for hydrogeologists to upload logger data'
 
 try:
     import pypandoc
@@ -18,23 +18,19 @@ try:
 except:
     pass
 
-setup(name='wellapplication',
-      description = 'Interface with xle files; analyze hydrographs; plot hydrographs; download USGS data',
+setup(name='loggerloader',
+      description = 'Interface with xle files; analyze hydrographs; plot hydrographs',
       long_description = long_description,
-      version = '0.4.26',
+      version = '0.1.0',
       author = 'Paul Inkenbrandt',
       author_email = 'paulinkenbrandt@utah.gov',
-      url = 'https://github.com/inkenbrandt/WellApplication',
+      url = 'https://github.com/inkenbrandt/loggerloader',
       license = 'LICENSE.txt',
       install_requires=["Pandas >= 0.16.0", 
                         "Numpy >= 0.7.0", 
                         "Matplotlib >= 1.1", 
                         "xmltodict >= 0.6.2",
-                        "scipy >= 0.10.0",
-                        "pyproj >= 1.9.4",
-                        "requests >= 2.11.1",
-                        "xlrd >= 0.5.4",
-                        "statsmodels >= 0.6.0"],
+                        "xlrd >= 0.5.4"],
       packages = find_packages(exclude=['contrib', 'docs', 'tests*']))
 
 
