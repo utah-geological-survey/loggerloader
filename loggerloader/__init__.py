9d9ee477ee82c5e68c2ce2cb1113c6ee75367a96
inkenbrandt <paulinkenbrandt@utah.gov>
2017-07-31 16:31:08 -0600

    added imp_one_well

--- loggerloader/__init__.py
+++ loggerloader/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*- 
 from __future__ import absolute_import, division, print_function, unicode_literals
-import os
 
-from .transport import *
+
+from .loggerloader import *
 
 __version__ = '0.2.2'
 __author__ = 'Paul Inkenbrandt'
