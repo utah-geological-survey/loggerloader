2700906e73c6c31c4c2a99e8b5406e1b2cd2f8a9
inkenbrandt <paulinkenbrandt@utah.gov>
2017-08-03 10:37:12 -0600

    fix import error

--- loggerloader/__init__.py
+++ loggerloader/__init__.py
@@ -1,7 +1,6 @@
 from loggerloader.transport import *
 from loggerloader.utilities import *
 from loggerloader.data_fixers import *
-from loggerloader.header_tables import *
 
 __version__ = "0.2.6"
 
