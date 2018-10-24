6a207f6ca61df56fc70cc3440504c24599928bfb
Paul <paulinkenbrandt@utah.gov>
2018-10-24 16:29:29 -0600

    update tests

---
+++ test/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+import os
+
+try:
+    from loggerloader.loggerloader import *
+except ImportError:
+    from .loggerloader import *
+
+__version__ = '0.4.3'
+__author__ = 'Paul Inkenbrandt'
+__name__ = 'loggerloader'
+
+__all__ = ['new_trans_imp','well_baro_merge','fcl','wellimport','simp_imp_well','WaterElevation',
+           'table_to_pandas_dataframe','HeaderTable','PullOutsideBaro']
